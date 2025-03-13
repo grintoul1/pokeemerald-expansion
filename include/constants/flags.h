f00dd9616dcee1f6b3c9e74800806d238cf8619d
grintoul1 <166724814+grintoul1@users.noreply.github.com>
2025-03-13 18:59:31 +0000

    20250313 01
    Changed Gym leaders back to talk-to-trigger events instead of event tiles
    Reverted Petalburg Gym layout and changed trainers back to talk-to-trigger events. Rooms now run off flags (need to test).
    Fixed Cycling Road South exit to only advance Route state on first exit on Mach Bike.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -656,13 +656,13 @@
 #define FLAG_HIDDEN_ITEM_ROUTE_123_RARE_CANDY                (FLAG_HIDDEN_ITEMS_START + 0x6E)
 #define FLAG_HIDDEN_ITEM_ROUTE_105_BIG_PEARL                 (FLAG_HIDDEN_ITEMS_START + 0x6F)
 
-#define FLAG_UNUSED_0x264  0x264 // Unused Flag
-#define FLAG_UNUSED_0x265  0x265 // Unused Flag
-#define FLAG_UNUSED_0x266  0x266 // Unused Flag
-#define FLAG_UNUSED_0x267  0x267 // Unused Flag
-#define FLAG_UNUSED_0x268  0x268 // Unused Flag
-#define FLAG_UNUSED_0x269  0x269 // Unused Flag
-#define FLAG_UNUSED_0x26A  0x26A // Unused Flag
+#define FLAG_MARY_DEFEATED      0x264 // Petalburg Gym trainer
+#define FLAG_RANDALL_DEFEATED   0x265 // Petalburg Gym trainer
+#define FLAG_GEORGE_DEFEATED    0x266 // Petalburg Gym trainer
+#define FLAG_ALEXIA_DEFEATED    0x267 // Petalburg Gym trainer
+#define FLAG_PARKER_DEFEATED    0x268 // Petalburg Gym trainer
+#define FLAG_BERKE_DEFEATED     0x269 // Petalburg Gym trainer
+#define FLAG_JODY_DEFEATED      0x26A // Petalburg Gym trainer
 #define FLAG_UNUSED_0x26B  0x26B // Unused Flag
 #define FLAG_UNUSED_0x26C  0x26C // Unused Flag
 #define FLAG_UNUSED_0x26D  0x26D // Unused Flag
